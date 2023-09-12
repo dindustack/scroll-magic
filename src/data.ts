479badb9642a6d34175f61adc7191e44bdc77829
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-09-12 11:18:07 +0100

    data partial fix

---
+++ src/data.ts
@@ -0,0 +1,64 @@
+export type Project = {
+  id: number;
+  projectName: string;
+  imgSrc: string;
+};
+
+export const projects: Project[] = [
+  {
+    id: 1,
+    projectName: "Giraffe",
+    imgSrc:
+      "https://images.unsplash.com/photo-1523805009345-7448845a9e53?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+  },
+  {
+    id: 2,
+    projectName: "South Africa",
+    imgSrc:
+      "https://images.unsplash.com/photo-1484318571209-661cf29a69c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 3,
+    projectName: "Working Mom",
+    imgSrc:
+      "https://images.unsplash.com/photo-1473605768212-7e1f2c756179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 4,
+    projectName: "Playing Kids",
+    imgSrc:
+      "https://images.unsplash.com/photo-1459183885421-5cc683b8dbba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 5,
+    projectName: "Ngorongoro Crater",
+    imgSrc:
+      "https://images.unsplash.com/photo-1566296524462-e0a341bf65e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
+  },
+
+  {
+    id: 6,
+    projectName: "African Girl",
+    imgSrc:
+      "https://images.unsplash.com/photo-1509099863731-ef4bff19e808?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+  },
+
+  {
+    id: 7,
+    projectName: "Ankara Fabric",
+    imgSrc:
+      "https://images.unsplash.com/photo-1552710307-537199cd41c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 8,
+    projectName: "Uganda Kids",
+    imgSrc:
+      "https://images.unsplash.com/photo-1521493959102-bdd6677fdd81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+  },
+  {
+    id: 9,
+    projectName: "Safari Sunset",
+    imgSrc:
+      "https://images.unsplash.com/photo-1516426122078-c23e76319801?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2068&q=80",
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+import Scrollbar from "smooth-scrollbar";
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
@@ -8,7 +10,36 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
           <use xlink:href="/icons.svg#ic-chevron-small-down"></use>
         </svg>
       </div>
+      <section class="content"></section>
     </div>
 `;
 
 // setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+
+const PADDING = 8;
+const ITEM_SIZE = 182.4 - PADDING;
+
+const select = (elem: string) => document.querySelector(elem) as HTMLElement;
+const selectAll = (elem: string): HTMLElement[] =>
+  Array.from(document.querySelectorAll(elem));
+const create = (elem: string): HTMLElement => document.createElement(elem);
+
+window.addEventListener("load", () => {
+  const content = select(".content");
+  const heading = select(".heading");
+
+  const verticalScrollbar = Scrollbar.init(content, {
+    damping: 0.03,
+    delegateTo: document,
+  });
+  verticalScrollbar.setPosition(0, 0);
+  verticalScrollbar.track.yAxis.element.remove();
+  verticalScrollbar.track.xAxis.element.remove();
+  verticalScrollbar.addListener(({ offset }) => {
+    if (offset.y > 50) {
+      gsap.to(heading, { opacity: 0 });
+    } else {
+      gsap.to(heading, { opacity: 1 });
+    }
+  });
+});
