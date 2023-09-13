2392e06f291a4c2d328a7f0e08fe545b0b974fbf
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2023-09-13 01:32:57 +0100

    project fix

--- src/data.ts
+++ src/data.ts
@@ -1,189 +1,189 @@
-export type Project = {
+export type Data = {
   id: number;
-  projectName: string;
+  title: string;
   imgSrc: string;
 };
 
-export const projects: Project[] = [
+export const data: Data[] = [
   {
     id: 1,
-    projectName: "Giraffe",
+    title: "Giraffe",
     imgSrc:
       "https://images.unsplash.com/photo-1523805009345-7448845a9e53?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
   },
   {
     id: 2,
-    projectName: "South Africa",
+    title: "South Africa",
     imgSrc:
       "https://images.unsplash.com/photo-1484318571209-661cf29a69c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 3,
-    projectName: "Working Mom",
+    title: "Working Mom",
     imgSrc:
       "https://images.unsplash.com/photo-1473605768212-7e1f2c756179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 4,
-    projectName: "Playing Kids",
+    title: "Playing Kids",
     imgSrc:
       "https://images.unsplash.com/photo-1459183885421-5cc683b8dbba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 5,
-    projectName: "Ngorongoro Crater",
+    title: "Ngorongoro Crater",
     imgSrc:
       "https://images.unsplash.com/photo-1566296524462-e0a341bf65e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
   },
 
   {
     id: 6,
-    projectName: "African Girl",
+    title: "African Girl",
     imgSrc:
       "https://images.unsplash.com/photo-1509099863731-ef4bff19e808?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
   },
 
   {
     id: 7,
-    projectName: "Ankara Fabric",
+    title: "Ankara Fabric",
     imgSrc:
       "https://images.unsplash.com/photo-1552710307-537199cd41c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 8,
-    projectName: "Uganda Kids",
+    title: "Uganda Kids",
     imgSrc:
       "https://images.unsplash.com/photo-1521493959102-bdd6677fdd81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 9,
-    projectName: "Safari Sunset",
+    title: "Safari Sunset",
     imgSrc:
       "https://images.unsplash.com/photo-1516426122078-c23e76319801?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2068&q=80",
   },
   {
     id: 10,
-    projectName: "African Craftwork",
+    title: "African Craftwork",
     imgSrc:
       "https://images.unsplash.com/photo-1556379069-7c1b1b8990b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2053&q=80",
   },
   {
     id: 11,
-    projectName: "Adjame Market",
+    title: "Adjame Market",
     imgSrc:
       "https://images.unsplash.com/photo-1552710307-8d1c604d6319?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2067&q=80",
   },
   {
     id: 12,
-    projectName: "Mozambique",
+    title: "Mozambique",
     imgSrc:
       "https://images.unsplash.com/photo-1506370612293-360919ece3d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2058&q=80",
   },
   {
     id: 13,
-    projectName: "Sierre Leone Women",
+    title: "Sierre Leone Women",
     imgSrc:
       "https://images.unsplash.com/photo-1509099896299-af46ad97ff57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
   },
   {
     id: 14,
-    projectName: "South Africa",
+    title: "South Africa",
     imgSrc:
       "https://images.unsplash.com/photo-1497271679421-ce9c3d6a31da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80",
   },
   {
     id: 15,
-    projectName: "Aït Benhaddou",
+    title: "Aït Benhaddou",
     imgSrc:
       "https://images.unsplash.com/photo-1536237717235-0acadb345d8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 16,
-    projectName: "Maasai Tribe",
+    title: "Maasai Tribe",
     imgSrc:
       "https://images.unsplash.com/photo-1547496727-11c450fe4e7f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 17,
-    projectName: "Cape Verde",
+    title: "Cape Verde",
     imgSrc:
       "https://images.unsplash.com/photo-1574931697692-f937f1a4134f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 18,
-    projectName: "African Women",
+    title: "African Women",
     imgSrc:
       "https://images.unsplash.com/photo-1574931697692-f937f1a4134f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 19,
-    projectName: "Freetown",
+    title: "Freetown",
     imgSrc:
       "https://images.unsplash.com/photo-1630509866818-4c7be20bb76d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 20,
-    projectName: "Kampala Boy",
+    title: "Kampala Boy",
     imgSrc:
       "https://images.unsplash.com/photo-1519032284022-0fdfbdb3c42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80",
   },
   {
     id: 21,
-    projectName: "Ghanian Girls",
+    title: "Ghanian Girls",
     imgSrc:
       "https://images.unsplash.com/photo-1511524511424-6fd4e055e7ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 22,
-    projectName: "African Cocoa",
+    title: "African Cocoa",
     imgSrc:
       "https://images.unsplash.com/photo-1511524511424-6fd4e055e7ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 23,
-    projectName: "Zebra Wildlife",
+    title: "Zebra Wildlife",
     imgSrc:
       "https://images.unsplash.com/photo-1515205244153-fce4e5d8bc49?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2152&q=80",
   },
   {
     id: 24,
-    projectName: "School Children",
+    title: "School Children",
     imgSrc:
       "https://images.unsplash.com/photo-1511524511424-6fd4e055e7ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 25,
-    projectName: "Street Hawker",
+    title: "Street Hawker",
     imgSrc:
       "https://images.unsplash.com/photo-1604072382401-b71031fa0315?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1975&q=80",
   },
   {
     id: 26,
-    projectName: "Kargi Tribe",
+    title: "Kargi Tribe",
     imgSrc:
       "https://images.unsplash.com/photo-1515657241610-a6b33f0f6c5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1952&q=80",
   },
   {
     id: 27,
-    projectName: "Madagascar Mother",
+    title: "Madagascar Mother",
     imgSrc:
       "https://images.unsplash.com/photo-1504598578017-40d9b776f1bc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 28,
-    projectName: "Desert Jeep",
+    title: "Desert Jeep",
     imgSrc:
       "https://images.unsplash.com/photo-1498038116800-4159eb9b2a62?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 29,
-    projectName: "Kabwe School",
+    title: "Kabwe School",
     imgSrc:
       "https://images.unsplash.com/photo-1610412970889-ed4509dc50c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 30,
-    projectName: "Tanzania Banana",
+    title: "Tanzania Banana",
     imgSrc:
       "https://images.unsplash.com/photo-1516047001178-6dcd2a01c694?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
   },
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import gsap from "gsap";
 import Scrollbar from "smooth-scrollbar";
+import { data } from "./data";
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
@@ -14,15 +15,13 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </div>
 `;
 
-// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
-
-// const PADDING = 8;
-// const ITEM_SIZE = 182.4 - PADDING;
+const PADDING = 8;
+const ITEM_SIZE = 182.4 - PADDING;
 
 const select = (elem: string) => document.querySelector(elem) as HTMLElement;
-// const selectAll = (elem: string): HTMLElement[] =>
-//   Array.from(document.querySelectorAll(elem));
-// const create = (elem: string): HTMLElement => document.createElement(elem);
+const selectAll = (elem: string): HTMLElement[] =>
+  Array.from(document.querySelectorAll(elem));
+const create = (elem: string): HTMLElement => document.createElement(elem);
 
 window.addEventListener("load", () => {
   const content = select(".content");
@@ -36,10 +35,122 @@ window.addEventListener("load", () => {
   verticalScrollbar.track.yAxis.element.remove();
   verticalScrollbar.track.xAxis.element.remove();
   verticalScrollbar.addListener(({ offset }) => {
+    const itemFull = selectAll(".item-full");
+
     if (offset.y > 50) {
       gsap.to(heading, { opacity: 0 });
     } else {
       gsap.to(heading, { opacity: 1 });
     }
+
+    gsap.to(itemFull, { y: -offset.y, duration: 0 });
   });
+
+  window.addEventListener("resize", () => setPositionCalculations());
+
+  const setPositionCalculations = () => {
+    const containerFull = select(".item-container-full");
+    const containerTransparent = select(".item-container-transparent");
+    const scrollContent = select(".scroll-content");
+
+    const scrollContentHeight = data.length * (ITEM_SIZE + PADDING);
+    const multiplyTime = window.innerHeight / (ITEM_SIZE + PADDING) - 2.5;
+
+    gsap.set(scrollContent, {
+      blockSize: scrollContentHeight + ITEM_SIZE * multiplyTime + ITEM_SIZE,
+    });
+    gsap.set(containerFull, { top: ITEM_SIZE * multiplyTime });
+    gsap.set(containerTransparent, { y: ITEM_SIZE * multiplyTime });
+  };
+
+  const generateList = () => {
+    const scrollContent = select(".scroll-content");
+    const picture = create("div");
+    const containerFull = create("div");
+    const containerTransparent = create("div");
+
+    gsap.set(picture, { className: "picture" });
+    gsap.set(containerFull, { className: "item-container-full" });
+    gsap.set(containerTransparent, { className: "item-container-transparent" });
+
+    content.appendChild(containerFull);
+    scrollContent.insertAdjacentElement("beforeend", containerTransparent);
+
+    data.forEach((item, index) => {
+      const image = create("img");
+      gsap.set(image, {
+        className: "img-select",
+        attr: { src: item.imgSrc, "data-id": item.id },
+        zIndex: index + 1,
+      });
+
+      picture.appendChild(image);
+      containerFull.appendChild(createItem(item, false));
+      containerTransparent.appendChild(createItem(item, true));
+    });
+
+    content.appendChild(picture);
+  };
+
+  const createItem = (
+    item: { title: string; id: number },
+    isTransparent = false
+  ) => {
+    const itemList = item.title.split(" ");
+
+    const itemContainer = create("div");
+    const title = create("div");
+
+    gsap.set(itemContainer, {
+      className: `item ${isTransparent ? "item-transparent" : "item-full"}`,
+      attr: { "data-id": item.id },
+    });
+
+    gsap.set(title, {
+      className: `title ${isTransparent ? "title-transparent" : "title-full"}`,
+      textContent: itemList[itemList.length - 1],
+    });
+
+    itemContainer.appendChild(title);
+
+    return itemContainer;
+  };
+
+  const initObserver = () => {
+    const pictureList = selectAll(".img-select");
+
+    let options = {
+      root: verticalScrollbar.containerEl,
+      rootMargin: `${ITEM_SIZE}px 0px -${ITEM_SIZE}px 0px`,
+      threshold: 0.4,
+    };
+
+    let observer = new IntersectionObserver((entries, _) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          animate(entry, pictureList, "in");
+        } else {
+          animate(entry, pictureList, "out");
+        }
+      });
+    }, options);
+
+    verticalScrollbar.containerEl
+      .querySelectorAll(".item-transparent")
+      .forEach((item) => observer.observe(item));
+  };
+
+  const animate = (entry: any, pictureList: any, direction: string) => {
+    const target = pictureList[entry.target.dataset.id - 1];
+
+    gsap.to(target, {
+      translateY: direction === "in" ? 0 : 500,
+      scale: direction === "in" ? 1 : 1.5,
+      duration: 0.5,
+    });
+  };
+
+  generateList();
+  setPositionCalculations();
+  initObserver();
 });
